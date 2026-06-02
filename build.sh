2450f3f13a1aee8e506902833319bce5f6f750ef
Aman <amanarora2004@gmail.com>
2026-06-02 14:41:22 -0700

    swipe 1

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ emcc main.c raytracing.c raycasting.c input.c -o index.html \
   -s USE_SDL=2 \
   -s WASM=1 \
   -s ALLOW_MEMORY_GROWTH=1 \
-  -s EXPORTED_FUNCTIONS='["_main"]' \
+  -s EXPORTED_FUNCTIONS='["_main","_set_touch_rotation"]' \
   -s EXPORTED_RUNTIME_METHODS='[]'
